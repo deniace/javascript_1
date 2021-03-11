d57199ae1dbc885482c5c9908160c92305369fc8
Deni Supriyatna <denisupriyatna01@gmail.com>
2021-03-11 09:35:19 +0700

    latihan selector element

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
-console.log(document);
+// console.log(document);
 
 // dom selection
-document.getElementById('judul');
+// document.getElementById('judul'); -> mengembalikan 1 element
+const judul = document.getElementById('judul');
+
+judul.style.color = 'red';
+judul.style.backgroundColor = '#ccd';
+judul.innerHTML = "Hellow deni ace"; 
+
+
+// document.getElementsByTagName -> mengembalikan HTML collection -> mirip array
+const p = document.getElementsByTagName('p');
+p[2].style.backgroundColor = 'lightBlue';
+
+for (let i = 0; i < p.length; i++) {
+    p[i].style.backgroundColor = 'orange';
+}
+
+
+// document.getElementsByClassName -> return nya html HTMLAllCollection
+// menggambil document berdasarkan class name
+
+// const p1 = document.getElementsByClassName('p1')[0]; bisa dikasi index di belakang
+const p1 = document.getElementsByClassName('p1');
+p1[0].innerHTML = "ini diubah dari javascript";
--- struktur-if.js
+++ struktur-if.js
@@ -5,14 +5,6 @@ var angka2 = document.getElementById("number2");
 var pangka1 = document.getElementById("pangka1");
 var pangka2 = document.getElementById("pangka2");
 
-btnCek.onclick = function () {
-    let a = angka1.value;
-    let b = angka2.value;
-    pangka1.innerText = a;
-    pangka2.innerText = b;
-    hasil.innerText = cekAngka(a,b);
-}
-
 function cekAngka(a,b) {
     let aa = "";
     if(a == b){
@@ -21,6 +13,17 @@ function cekAngka(a,b) {
         aa = "a lebih besar dari b";
     }else if (a < b) {
         aa= "B lebih besar dari a";
+    }else{
+        aa = "lah";
     }
     return aa;
 }
+
+btnCek.onclick = function () {
+    let a = angka1.value;
+    let b = angka2.value;
+    pangka1.innerText = a;
+    pangka2.innerText = b;
+    hasil.innerText = cekAngka(a,b);
+}
+
