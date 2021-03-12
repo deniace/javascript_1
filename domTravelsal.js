220115825123818b2316ef2807d73d425c1031e1
Deni Supriyatna <denisupriyatna01@gmail.com>
2021-03-12 22:20:25 +0700

    dom travelsal

---
+++ domTravelsal.js
@@ -0,0 +1,29 @@
+// const tutup = document.querySelector('.close');
+
+// tutup.addEventListener('click',function(){
+//     const divCard = document.querySelector('.card');
+//     divCard.outerHTML = "";
+// });
+
+
+// dom travelsa;
+
+const tutup = document.querySelectorAll('.close');
+
+// for (let i = 0; i < tutup.length; i++) {
+//     tutup[i].addEventListener('click', function(e){
+//         // tutup[i].parentElement.style.display = 'none';
+//         e.target.parentElement.style.display = 'none';
+//     });
+    
+// }
+
+tutup.forEach(function(el){
+    el.addEventListener('click', function(e){
+        e.target.parentElement.style.display = 'none';
+    });
+});
+
+const nama = document.querySelector('.nama');
+
+console.log(nama.parentElement.nextElementSibling.firstElementChild.nextElementSibling);
